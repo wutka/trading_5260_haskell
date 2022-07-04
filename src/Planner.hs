c580c854c129c9a72436bb006d1f1f80bd7c54f3
Mark Wutka <mark.wutka@vanderbilt.edu>
2022-07-04 10:40:24 -0500

    Added planner

---
+++ src/Planner.hs
@@ -0,0 +1,14 @@
+module Planner where
+
+import Resources
+
+
+--def my_country_scheduler (your_country_name, resources_filename,
+--                         initial_state_filename,  output_schedule_filename,
+--                         num_output_schedules, depth_bound, frontier_max_size)
+
+computeSchedule :: ResourceMap -> Int -> Int -> Int -> [[ScheduleItem]]
+computeSchedule resourceMap numOutputSchedules depthBound frontierMaxSize =
+
+
+
