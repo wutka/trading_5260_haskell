6ca2a18b78d3f3819493715857d9e1b9d59ea310
Mark Wutka <mark.wutka@vanderbilt.edu>
2022-08-08 19:22:07 -0500

    Added comments to Main

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import System.Environment ( getArgs )
 import Data.List
 import qualified Data.Map as Map
 
+-- Prints the results for a single country in a single round
 printRoundResult :: (Map.Map String PlannerConfig) -> RoundResult -> IO ()
 printRoundResult pcMap (RoundResult country score cr details updates autoTransforms schedule) = do
   putStrLn $ country++": "++show score
@@ -20,26 +21,58 @@ printRoundResult pcMap (RoundResult country score cr details updates autoTransfo
   putStrLn $ unlines $ map show autoTransforms
   putStrLn "Schedule:"
   putStrLn $ unlines $ showSchedule schedule
+  
   where
+    -- Extract the country's resource data from the resource data map
     countryRm = cr Map.! country
+
+    -- Convert the resources to a string of name-value pairs
     countryResourcesStr = unwords $ map showRes $ Map.toList countryRm
+
+    -- Represent a resource and value as name:value
     showRes (s,i) = s ++ ":" ++ show i
+
+    -- Represent an updated resource as update type: resource = amount
     showUpdate (UpdatedResource d s i) = d ++ ": " ++ s ++ "=" ++ show i
+
+    -- If there was no valid schedule, just show "No Valid Schedule"
     showSchedule Nothing = ["No valid schedule"]
+
+    -- Otherwise, turn each item in the schedule into a string
     showSchedule (Just schedule) = map show schedule
+
+    -- Represent a scoring detail as the scoringType field val
     scoreDetail (ScoreDetail scoreType field val) = scoreType ++" "++field++" "++show val
-    scoreDetails = map scoreDetail details    
+
+    -- Turn the scoring details into a list of strings
+    scoreDetails = map scoreDetail details
+
+    -- Retrieves only the scoring element from a PlannerConfig structure
     getScoring (PlannerConfig _ _ _ _ _ _ scoring _ _ _) = scoring
+
+    -- Get the scoring for this country from the PlannerConfig
     scoring = getScoring $ pcMap Map.! country
-    
+
+-- Prints the results of a single round    
 printRound :: (Map.Map String PlannerConfig) -> [String] -> (Int,Map.Map String RoundResult) -> IO ()
 printRound pcMap countries (round,resultMap) = do
+  -- Print the round number
   putStrLn $ "Round: "++show round
+
+  -- Call printRoundResult for each member of roundResults
   mapM_ (printRoundResult pcMap) roundResults
+  
   where
+
+    -- Fetch the round results for each country
     roundResults = map lookupCountry countries
+
+    -- Fetch the round results for a specific country
     lookupCountry country = resultMap Map.! country
 
+-- At startup we try to verify all the transforms to make sure they contain
+-- valid resource names. This function checks to make sure a resource is
+-- contained in the initial resource map
 checkResource :: Map.Map String Int -> ResourceAmount -> IO ()
 checkResource rm (ResourceAmount res _) = do  
   if Map.notMember res rm then
@@ -47,29 +80,59 @@ checkResource rm (ResourceAmount res _) = do
   else
     return ()
 
+-- For a transform, make sure all the inputs and outputs contain valid fields
+-- Note: Since Transfer structs are not read in from external files, we don't
+-- need a corresponding check for them
 checkTransform :: Map.Map String Int -> Transform -> IO ()
 checkTransform rm (Transform country inputs outputs) = do
   mapM_ (checkResource rm) inputs
   mapM_ (checkResource rm) outputs
 
+-- Loads all the files needed for the PlannerConfig for a single country
+-- The country data files are expected to be in data/countries with specific names
+-- for each file type.
 loadPlannerConfig :: CountryResources -> String -> IO PlannerConfig
 loadPlannerConfig cr country = do
+  -- Load the transforms that the AI can do
   transforms <- loadTransforms (countryDir++"transforms")
+
+  -- Load the transforms that run automatically each turn
   autoTransforms <- loadTransforms (countryDir++"auto_transforms")
+
+  -- Load the scoring rules (each country can have its own scoring)
   scoring <- loadScoringFormula (countryDir++"scoring.txt")
+
+  -- Load the rules for updating resources each turn (any updates
+  -- that can affect multiple resources are defined as auto_transforms
+  -- instead.
   updates <- loadUpdates (countryDir++"updates.txt")
+
+  -- Load other config settings, currently maxDepth (depthBound), frontierSize,
+  -- numSchedules, and gamma
   items <- loadConfigFile (countryDir++"config.txt")
 
+  -- Make sure the transforms all have valid field names
   let rm = cr Map.! country
   mapM_ (checkTransform rm) transforms
   mapM_ (checkTransform rm) autoTransforms
 
+  -- Create a basic PlannerConfig, and then iterate through the items from config.txt, updating
+  -- the specified field
   return $ foldl' handleConfigLine (PlannerConfig country otherCountries 4 200 1 1.0 scoring transforms autoTransforms updates) items
+  
   where
+
+    -- Country data files are in a specific place
     countryDir = "data/countries/"++country++"/"
+
+    -- Extract the list of countries to use from the resources structure
     countries = Map.keys cr
+
+    -- Remove this country from the list of countries to create a list of the "other" countries
     otherCountries = delete country countries
 
+-- For each item from config.txt, look at the name of the item, and update PlannerConfig
+-- with the specified value.
 handleConfigLine :: PlannerConfig -> (String, CsvItem) -> PlannerConfig
 handleConfigLine pc ("maxDepth", CsvInt maxDepth) = pc { depthBound=maxDepth }
 handleConfigLine pc ("maxDepth", _) = error "Invalid data type for maxDepth, expected int"
@@ -89,18 +152,6 @@ main = do
   let turns = if length argv > 0 then read (head argv) else 10
   let resourcesFile = if length argv > 1 then argv !! 1 else "data/resources_part2.csv"
 
-  let transformsDir = if length argv > 0 then head argv else "data/transforms"
-  let autoTransformsDir = if length argv > 1 then argv !! 1 else "data/auto_transforms"
-  let updatesFile = if length argv > 2 then argv !! 2 else "data/updates.txt"
-  let scoringFile = if length argv > 3 then argv !! 3 else "data/scoring2.txt"
-
-  
-
-  let maxDepth = if length argv > 6 then read (argv !! 6) else 4
-  let frontierSize = if length argv > 7 then read (argv !! 7) else 200
-  let numSchedules = if length argv > 8 then read (argv !! 8) else 1
-  let gamma = if length argv > 9 then read (argv !! 9) else 1.0
-
   -- Load the country resources
   (countries,nontransfer) <- loadCountryResources resourcesFile
   plannerConfigs <- mapM (loadPlannerConfig countries) (Map.keys countries)
@@ -113,4 +164,3 @@ main = do
 --  putStrLn $ "[" ++ unlines (map show schedule) ++ "]"
   where
     plannerCountryPair pc@(PlannerConfig country _ _ _ _ _ _ _ _ _) = (country,pc)
-  
